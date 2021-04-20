31a1c95363a1893affb08c0a30e406d3609634c6
cithis <citlaliordunoun@gmail>
2021-04-19 19:34:42 -0600

    jar de mysql connector

---
+++ src/java/AccesoDatos/PreparatoriaDAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccesoDatos;
+
+import Dominio.Preparatoria;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Citlali Orduño
+ */
+public class PreparatoriaDAO extends BaseDAO<Preparatoria> {
+
+    @Override
+    public void agregar(Preparatoria entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actualizar(Preparatoria entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Preparatoria> consultarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Preparatoria buscarPorId(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
